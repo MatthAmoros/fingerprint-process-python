46bbb7ef353415a3ec177de4d3b8cf714a0a6b60
SunPaz <matth.amoros@gmail.com>
2018-07-03 11:54:24 -0700

    Cross number + validation

--- fingerprint-processing.py
+++ fingerprint-processing.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 def extract_skeleton(img):
 	"""
@@ -12,7 +13,7 @@ def extract_skeleton(img):
 	temp = np.zeros(img.shape, np.uint8)
 	thresh = img.copy()
 
-	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+	kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
 
 	iters = 0
 	while(True):
@@ -24,22 +25,9 @@ def extract_skeleton(img):
 
 		iters += 1
 		if cv2.countNonZero(thresh) == 0:
-			return (skeleton,iters)
+			return skeleton
 
-def get_contours_orientation(img):
-	"""
-	Get contours orientation
-	"""
-	im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-	"""
-	for i in range(len(contours)):
-		cnt = contours[4]
-		cv2.drawContours(img, [cnt], 0, (0,255,0), 3)
-	"""
-	cv2.drawContours(img, contours, -1, (0,255,0), 3)
-	return im2
-
-def crop_image_square(img, crop_size=5):
+def crop_image_square(img, crop_size=3):
 	"""
 	Crop image into smaller windows
 	@param crop_size = window size
@@ -47,8 +35,8 @@ def crop_image_square(img, crop_size=5):
 	"""
 	crop_collection = []
 
-	for r in range(0, img.shape[0] - crop_size, crop_size):
-		for c in range(0, img.shape[1] - crop_size, crop_size):
+	for r in range(0, img.shape[0]):
+		for c in range(0, img.shape[1]):
 			window = img[r:r+crop_size, c:c+crop_size]
 			crop_collection.append(window)
 
@@ -61,20 +49,143 @@ def pre_process(img):
 	@return : 300x300 binarized image
 	"""
 	## Resize to squared image
-	img = cv2.resize(img, (400, 400))
-	## Compute binary image with an adaptative threshold
-	thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+	img = cv2.resize(img, (300, 300))
+	_, img = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-	return thresh
+	return img
+
+def get_crossnumber_map(img):
+	"""
+	Extract pattern from binarized image
+	@param img : binarized image
+	@return matrix of weight
+	"""
+	image_x = img.shape[0]
+	image_y = img.shape[1]
+
+	print("Input image : " + str(image_x) + "x" + str(image_y))
+
+	local_compute = np.array([[1, 1,1],\
+									  [1,1,1],\
+									  [1,1,1]])
+
+	_, thresh = cv2.threshold(img,127,1,cv2.THRESH_BINARY)
+	filtered = cv2.filter2D(thresh,-1,local_compute)
+	cn_map = np.zeros(thresh.shape, np.uint8)
+	cn_hist = np.zeros([5])
+
+
+	""" Using cross number to detect minutiae """
+	for r in range(cn_map.shape[0]):
+		for c in range(cn_map.shape[1]):
+			if (r > 1 and c > 1) and (r < 299 and c < 299):
+				p = thresh[r][c]
+
+				p1 = thresh[r][c+1]
+				p2 = thresh[r-1][c+1]
+				p3 = thresh[r-1][c]
+
+				p4 = thresh[r-1][c-1]
+				p5 = thresh[r][c-1]
+				p6 = thresh[r+1][c-1]
+
+				p7 = thresh[r+1][c]
+				p8 = thresh[r+1][c+1]
+				p9 = p1
+				sum = 0
+
+				sum += math.fabs(p1 - p2)
+				sum += math.fabs(p2 - p3)
+				sum += math.fabs(p3 - p4)
+				sum += math.fabs(p4 - p5)
+				sum += math.fabs(p5 - p6)
+				sum += math.fabs(p6 - p7)
+				sum += math.fabs(p7 - p8)
+				sum += math.fabs(p8 - p9)
+				cn = 0.5 * sum
+
+				##NOTE : OUTPUT is > 128, why ?
+				cn = math.ceil(cn / 128.0)
+
+				"""
+				1 == Ridge ending point
+				2 == Continuing ridge point
+				3 == bifurcation point
+				4 == crossing point
+				"""
+				""" Using local neighbors to validate minutiae """
+				validated = False
+				weight = filtered[r][c]
+
+				if cn == 1:
+					validated = weight == 1
+				elif cn == 2:
+					validated = weight == 3
+				elif cn == 3:
+					validated = weight >= 3 or weight <= 5
+				elif cn == 4:
+					print(weight)
+
+				if validated:
+					cn_hist[cn] += 1
+					cn_map[r][c] = cn
+
+			else:
+				cn_map[r][c] = 0
+
+
+	print("Cross number hist : " + str(cn_hist))
+	return cn_hist, cn_map
 
 if __name__ == "__main__":
-	img = cv2.imread('./samples/001_01_0055.png', 0)
+	img = cv2.imread('./samples/001_01_0042.png', 0)
+
+	colorized_output = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+	colorized_output = cv2.resize(colorized_output, (300, 300))
+
 	binarized = pre_process(img)
-
-	cropped_images = crop_image_square(img)
-
-	sobely = cv2.Sobel(img,cv2.CV_32F,0,1,ksize=5)
 
 	cv2.imwrite('./fingerprint_extract/0_pre_proc.png', binarized)
 
-	cv2.imwrite('./fingerprint_extract/2_sobely.png', sobely)
+	thinned = cv2.ximgproc.thinning(binarized)
+
+	cv2.imwrite('./fingerprint_extract/1_thinned.png', thinned)
+
+	cn_hist, cn_map = get_crossnumber_map(thinned)
+
+	cv2.imwrite('./fingerprint_extract/2_detections.png', cn_map)
+
+	circle_color = (0,0,0)
+	for r in range(cn_map.shape[0]):
+		for c in range(cn_map.shape[1]):
+			minutiae_x = c
+			minutiae_y = r
+			circle_color = (0,0,0)
+			detected_feature = False
+			"""
+			According to crossing number algorithm :
+
+			1 == Ridge ending point
+			2 == Continuing ridge point
+			3 == bifurcation point
+			4 == crossing point
+			"""
+			if cn_map[r][c] > 0:
+				if cn_map[r][c] == 1:
+					circle_color = (0,255,0)
+					detected_feature = False ##DEBUG : Disabled
+				if cn_map[r][c] == 2:
+					circle_color = (255,0,0)
+					detected_feature = False ##DEBUG : Disabled
+				if cn_map[r][c] == 3:
+					circle_color = (0,0,255)
+					detected_feature = True
+				elif cn_map[r][c] == 4:
+					circle_color = (255,255,0)
+					detected_feature = True
+
+				if detected_feature:
+					cv2.circle(colorized_output, (minutiae_x, minutiae_y), 10, circle_color, 1)
+
+
+	cv2.imwrite('./fingerprint_extract/3_detected_minutiae.png', colorized_output)
